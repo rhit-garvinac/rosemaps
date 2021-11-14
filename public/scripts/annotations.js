a785bdc6752f81683eb7a681f3ac78d9b23ab49f
rhit-battleal <70907435+rhit-battleal@users.noreply.github.com>
2021-11-13 22:33:20 -0500

    Add files via upload

--- public/scripts/annotations.js
+++ public/scripts/annotations.js
@@ -3,33 +3,231 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Andre Battle
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
 /** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_ANNOTATIONS = "annotations";
+rhit.FB_KEY_TITLE = "title";
+rhit.FB_KEY_DESC = "desc";
+rhit.FB_KEY_AUTHOR = "author";
+rhit.FB_KEY_POSITION = "position";
+rhit.FB_KEY_BUILDING = "building";
+rhit.FB_COLLECTION_USERS = "users";
+rhit.fbAnnotationsManager = null;
+rhit.fbAnnotationManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
-rhit.ClassName = class {
-	constructor() {
-		
+rhit.Annotation = class {
+	constructor(id, author, title, building, desc, position){
+		this.id = id;
+		this.author = author;
+		this.title = title;
+		this.building = building;
+		this.desc = desc;
+		this.position = position;
 	}
-	methodName() {
+}
 
+rhit.FbAnnotationsManager = class {
+	constructor() {
+		this._documentSnapshots = [];
+		this._unsubscribe = null;
+
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_ANNOTATIONS);
+	}
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.orderBy(rhit.FB_KEY_BUILDING, "desc")
+			.limit(50).onSnapshot((querySnapshot) => {
+				this._documentSnapshots = querySnapshot.docs;
+				if (changeListener) {
+					changeListener();
+				}
+			});
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+	update(title, desc) {}
+	delete(desc) {}
+	get length() {
+		return this._documentSnapshots.length;
+	}
+	getAnnotationAtIndex(index) {
+		const doc = this._documentSnapshots[index];
+		return new rhit.Annotation(doc.id, doc.get(rhit.FB_KEY_AUTHOR), doc.get(rhit.FB_KEY_TITLE), 
+			doc.get(rhit.FB_KEY_BUILDING), doc.get(rhit.FB_KEY_DESC), doc.get(rhit.FB_KEY_POSITION));
+	}
+}
+
+rhit.ListPageController = class {
+	constructor() {
+
+		rhit.fbAnnotationsManager.beginListening(this.updateList.bind(this));
+
+		$("#editAnnotationDialog").on("shown.bs.modal", () => {
+			document.querySelector("#inputEditTitle").focus();
+		});
+
+		document.querySelector("#submitEditAnnotation").addEventListener("click", (event) => {
+			const title = document.querySelector("#inputEditTitle").value;
+			const desc = document.querySelector("#inputEditDesc").value;
+			rhit.fbAnnotationManager.update(title, desc);
+		});
+
+		document.querySelector("#submitDeleteAnnotation").addEventListener("click", (event) => {
+			rhit.fbAnnotationManager.delete().then(() => {
+				const urlParams = new URLSearchParams(window.location.search);
+				window.location.href = "/annotations.html?user=" + urlParams.get("user");; 
+			});;
+		});
+
+	}
+	updateList() {
+		const newList = htmlToElement("<div id='annotationListContainer'></div>")
+		for (let k = 0; k < rhit.fbAnnotationsManager.length; k++) {
+			const annotation = rhit.fbAnnotationsManager.getAnnotationAtIndex(k);
+			const urlParams = new URLSearchParams(window.location.search);
+			const user = urlParams.get("user");
+			var newCard;
+			if (user == null){
+				newCard = this._createCard(annotation);
+				newCard.onclick = (event) => {
+					rhit.fbAnnotationManager = new rhit.FbAnnotationManager(annotation.id);
+					rhit.fbAnnotationManager.beginListening(function() {return null});
+				};
+			} 
+			
+			if(user == annotation.author) {
+				newCard = this._createEditCard(annotation);
+				newCard.onclick = (event) => {
+					rhit.fbAnnotationManager = new rhit.FbAnnotationManager(annotation.id);
+					rhit.fbAnnotationManager.beginListening(function() {return null});
+				};
+			}
+			newList.appendChild(newCard);
+
+		}
+
+		const oldList = document.querySelector("#annotationListContainer");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+		oldList.parentElement.appendChild(newList);
+	}
+
+	_createCard(annotation) {
+		return htmlToElement(`<div id="${annotation.id}" class="card">
+			<div class="d-flex justify-content-between">
+				<div class="card-info">
+					<h2 class="card-room">Title: ${annotation.title}</h2>
+					<div class="card-author">Author: ${annotation.author}</div>
+					<div class="card-desc">Building: ${annotation.building}</div>
+					<div class="card-desc">Description: ${annotation.desc}</div>
+		
+				</div>
+			</div>
+	</div>`);
+	}
+
+	_createEditCard(annotation) {
+		return htmlToElement(`<div id="${annotation.id}" class="card">
+			<div class="d-flex justify-content-between">
+				<div class="card-info">
+					<h2 class="card-room">Title: ${annotation.title}</h2>
+					<div class="card-author">Author: ${annotation.author}</div>
+					<div class="card-desc">Building: ${annotation.building}</div>
+					<div class="card-desc">Description: ${annotation.desc}</div>
+		
+				</div>
+				<div class="card-buttons">
+					<button type="button" class="btn bmd-btn-fab card-btn" data-toggle="modal" data-target="#editAnnotationDialog">
+						<i class="material-icons">edit</i>
+					</button>
+					<button type="button" class="btn bmd-btn-fab card-btn" data-toggle="modal" data-target="#deleteAnnotationDialog">
+						<i class="material-icons">delete</i>
+					</button>
+				</div>
+			</div>
+	</div>`);
+	}
+}
+
+
+rhit.FbAnnotationManager = class {
+	constructor(annotationId) {
+		this._documentSnapshot = {};
+		this._unsubscribe = null;
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_ANNOTATIONS).doc(annotationId);
+	}
+
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.onSnapshot((doc) => {
+			if (doc.exists) {
+				this._document = doc;
+				changeListener();
+			} else {
+				console.log("Document does not exist any longer.");
+				console.log("CONSIDER: automatically navigate back to the home page.");
+			}
+		});
+	}
+
+	stopListening() {
+		this._unsubscribe();
+	}
+	get author() {
+		return this._document.get(rhit.FB_KEY_AUTHOR);
+	}
+
+	get title() {
+		return this._document.get(rhit.FB_KEY_TITLE);
+	}
+
+	get building() {
+		return this._document.get(rhit.FB_KEY_BUILDING);
+	}
+
+	get desc() {
+		return this._document.get(rhit.FB_KEY_DESC);
+	}
+	
+	get position() {
+		return this._document.get(rhit.FB_KEY_POSITION);
+	}
+
+
+	update(title, desc) {
+		this._ref.update({
+			[rhit.FB_KEY_AUTHOR]: firebase.auth().currentUser.uid,
+			[rhit.FB_KEY_TITLE]: title,
+			[rhit.FB_KEY_BUILDING]: this.building,
+			[rhit.FB_KEY_DESC]: desc,
+			[rhit.FB_KEY_POSITION]: this.position,
+		}).then(() => {
+			console.log("Document has been updated");
+		});
+	}
+	delete() {
+		return this._ref.delete();
 	}
 }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
-	console.log("Ready");
-}
+	if (document.querySelector("#listPage")) {
+		rhit.fbAnnotationsManager = new rhit.FbAnnotationsManager();
+		new rhit.ListPageController();
+	}
+};
 
 rhit.main();
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,4 @@
+
 /**
  * @fileoverview
  * Provides the JavaScript interactions for all pages.
@@ -11,60 +12,107 @@ var rhit = rhit || {};
 
 /** globals */
 rhit.FB_COLLECTION_ANNOTATIONS = "annotations";
-rhit.FB_KEY_ROOM = "room";
+rhit.FB_KEY_TITLE = "title";
 rhit.FB_KEY_DESC = "desc";
 rhit.FB_KEY_AUTHOR = "author";
+rhit.FB_KEY_POSITION = "position";
+rhit.FB_KEY_BUILDING = "building";
+rhit.FB_COLLECTION_USERS = "users";
 //rhit.FB_KEY_LAST_TOUCHED ="lastTouched";
 rhit.mapPageController = null;
-
+rhit.AuthManager = null;
+rhit.UserManaer = null;
 rhit.scaleFactor = 22/30; //0.73333333333333...
 
-/** nodes */
-//nodes[0] = floor 1 nodes, nodes[1] = floor 2 nodes, etc.
-rhit.nodes = [
-	[
-		[916, 419, "O111"],
-		[1617, 419, "O113"],
-		[2121, 415, "O115"]
-	],
-	[
-		//O227 (1340, 550) entrance
-		//O227 (1205, 450) center
-		[1205, 450, "O227"],
-		[1534, 450, "O229"],
-		[1855, 445, "O231"],
-		[2197, 440, "O233"]
-	]
-	//TODO 😔
-];
-
-rhit.floor = 2;
 
 /** classes and stuff */
 rhit.MapPageController = class {
 	constructor() {
-		// Make the map image element draggable:
-		rhit.dragElement(document.getElementById("map-viewport"));
-		// TODO Make other elems draggable??
-		//
+		const controller = this;
 		this._documentSnapshots = [];
-	  	this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_ANNOTATIONS);
-	  	this._unsubscribe = null;
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_ANNOTATIONS);
+		this._unsubscribe = null;
+		// Instantiate EasyZoom instances
+		var $easyzoom = $('.easyzoom').easyZoom();
+		var zoomed = false; var addComment = false;
+		// Get an instance API
+		var api = $easyzoom.filter('.easyzoom--with-toggle').data('easyZoom');
+		 
+		const urlParams = new URLSearchParams(window.location.search);
+		const buildingName = urlParams.get("buildings");
+		var elem = htmlToElement(`<div href="images/${buildingName}.png" 
+					data-standard="images/${buildingName}.png">
+					<img class="toggle" id="mainMap" 
+					src="images/${buildingName}.png" alt="map"> </div>`);
+		elem.href = `images/${buildingName}.png`;
+		api.swap(elem.dataset.standard, elem.href);
+		api.teardown();
 
+		$("#addAnnotationDialog").on("show.bs.modal", () => {
+			document.querySelector("#inputTitle").value = "";
+			document.querySelector("#inputDesc").value = "";
+		});
+
+		$("#addAnnotationDialog").on("shown.bs.modal", () => {
+			document.querySelector("#inputTitle").focus();
+		});
+		var title = null, desc = null;
+		document.querySelector("#submitAddAnnotation").addEventListener("click", (event) => {
+			title = document.querySelector("#inputTitle").value;
+			desc = document.querySelector("#inputDesc").value;
+		});
+
+		$('.toggle').on('click', function(e) {
+			if (addComment) {
+				this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_ANNOTATIONS);
+				var posX = $(this).offset().left, posY = $(this).offset().top;
+				posX = e.pageX - posX, posY = e.pageY - posY;
+				var scaleX = Math.round((posX/$(this).width()) * 100), scaleY = Math.round((posY/$(this).height()) * 100);
+				const urlParams = new URLSearchParams(window.location.search);
+				
+				if (title) {
+					console.log(rhit.AuthManager.uid);
+					this._ref.add({
+						[rhit.FB_KEY_AUTHOR]: rhit.AuthManager.uid,
+						[rhit.FB_KEY_TITLE]: title,
+						[rhit.FB_KEY_BUILDING]: urlParams.get("buildings"),
+						[rhit.FB_KEY_DESC]: desc,
+						[rhit.FB_KEY_POSITION]: [scaleY, scaleX]
+					})
+					.then(function (docRef) {
+						console.log("Document added with ID: ", docRef.id);
+						title = null, desc = null;
+						controller.displayNodes();
+					})
+					.catch(function (error) {
+						console.error("Error adding document: ", error);
+					});
+					var mapOverlay = document.getElementsByClassName("bmd-layout-content")[0];
+					var dropDown = document.getElementById("drop-down");
+					dropDown.style.opacity = 1;
+					mapOverlay.removeChild(mapOverlay.lastChild);
+					addComment = false;
+				}
+
+		   }
+		   else {
+			   if (zoomed == false) {
+				   zoomed = true;
+				   api._init();
+			   } else {
+				   zoomed = false;
+				   api.teardown();
+			   }
+		   }
+		   });
+		
 		document.querySelector("#fab").addEventListener("click",(event) => {
-			var map = document.getElementById("map-img")
-			var currSrc = map.src;
-			console.log(currSrc);
-			if(currSrc.indexOf("images/Olin2.png") != -1){
-				map.src = currSrc.replace("images/Olin2.png", "images/Olin1.png");
-				rhit.floor = 1;
-			} else if(currSrc.indexOf("images/Olin1.png") != -1){
-				map.src = currSrc.replace("images/Olin1.png", "images/Olin2.png");
-				rhit.floor = 2;
-			} else {
-				console.error("images " + currSrc + " not found");
-			}
-			this.displayNodes();
+			var elem = htmlToElement(`<div id="clickpane"> Click anywhere to add an Annotation </div>`);
+			var mapOverlay = document.getElementsByClassName("bmd-layout-content")[0];
+			var dropDown = document.getElementById("drop-down");
+			dropDown.style.opacity = 0;
+			mapOverlay.appendChild(elem);
+			addComment = true;
 		});
 
 		this.displayNodes();
@@ -72,108 +120,217 @@ rhit.MapPageController = class {
 
 	displayNodes(){
 		//display nodes
-		const mapdiv = document.getElementById("map-viewport");
-		while (mapdiv.childNodes.length > 3) {
-			//children we don't remove: text node, img node, text node
-			mapdiv.removeChild(mapdiv.lastChild);
+		const annotations = document.getElementById("annotationList");
+		while (annotations.childNodes.length != 0) {
+			annotations.removeChild(annotations.lastChild);
 		}
-		rhit.nodes[rhit.floor - 1].forEach(node => 
-		{//firebase room number query --> array of annotations for that room
-		let query = this._ref.where(rhit.FB_KEY_ROOM, "==", node[2]);
-		var annotations = [];
-		query.onSnapshot((querySnapshot) => {
-			querySnapshot.forEach((doc) => {
+		this._ref.get().then((snapshot) => {
+			snapshot.docs.map(doc => {
 				var annotation = new rhit.Annotation(
-					doc.id,
-					doc.get(rhit.FB_KEY_ROOM),
-					doc.get(rhit.FB_KEY_DESC)
+					doc.get(rhit.FB_KEY_AUTHOR),
+					doc.get(rhit.FB_KEY_TITLE),
+					doc.get(rhit.FB_KEY_BUILDING),
+					doc.get(rhit.FB_KEY_DESC),
+					doc.get(rhit.FB_KEY_POSITION)
 				);
-				annotations.push(annotation);
-			});
-
-			//IMPORTANT NOTE: Do NOT give any html elems a room id e.g. id="O227"
-			//These ids will be used programmatically by this function
-			let elem = null;
-
-			if(annotations.length == 0){
-				elem = htmlToElement(`<button id=${node[2]} class="btn" style="position:absolute; top:${node[1]}px; left:${node[0]}px">
-								<i class="material-icons">place</i>
-							</button>`);
-			} else {
-				elem = htmlToElement(`<button id=${node[2]} class="btn" style="position:absolute; top:${node[1]}px; left:${node[0]}px">
-								<i class="material-icons">comment</i>
-							</button>`);
-				elem.addEventListener("click", function(e){console.log(annotations);});
-			}
-			mapdiv.appendChild(elem);
+				const urlParams = new URLSearchParams(window.location.search);
+				if (annotation.building == urlParams.get("buildings")){
+					console.log(doc);
+					var elem = htmlToElement(`<button class="btn btn-color" data-toggle="modal"
+					data-target="#viewAnnotation" style="position:absolute;
+					top:${annotation.position[0]}%; left:${annotation.position[1]}%; z-index: 100;">
+					<i class="material-icons">comment</i> </button>`);
+					elem.addEventListener("click", function(e){
+						document.querySelector("#annotationTitle").innerText = annotation.title;
+						document.querySelector("#annotationDesc").innerText = annotation.desc;
+						document.querySelector("#annotationAuth").innerText = annotation.author;
+					});
+					annotations.appendChild(elem);
+				}
 			});
 		});
 	}
 }
 
-//Draggable Element code from https://www.w3schools.com/howto/howto_js_draggable.asp
-rhit.dragElement = function(elmnt) {
-  var deltaX = 0, deltaY = 0, prevX = 0, prevY = 0;
-	elmnt.onmousedown = dragMouseDown;
-	elmnt.addEventListener("touchstart",  function(e) {dragTouchDown(e)});
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    prevX = e.clientX;
-    prevY = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementClickDrag;
+rhit.SideNavController = class {
+  constructor(){
+	const allAnnotations = document.querySelector("#allAnnotations");
+		if (allAnnotations) {
+			allAnnotations.addEventListener("click", (event) => {
+				window.location.href = "/annotations.html";
+			});
+		}
+	const myAnnotations = document.querySelector("#myAnnotations");
+		if (myAnnotations) {
+			myAnnotations.addEventListener("click", (event) => {
+				window.location.href = "/annotations.html?user=" + rhit.AuthManager.uid;
+			});
+		}
+    const menuSignOutItem = document.querySelector("#menuSignOut");
+ 		if (menuSignOutItem) {
+ 			menuSignOutItem.addEventListener("click", (event) => {
+ 				rhit.AuthManager.signOut();
+ 			});
+ 		}
   }
+}
 
-  function dragTouchDown(e) {
-	e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-	touch = e.touches[0];
-    prevX = touch.clientX;
-    prevY = touch.clientY;
-	document.ontouchend = closeDragElement;
-    // call a function whenever the cursor moves:
-	document.ontouchmove = elementTouchDrag;
-  }
+rhit.LoginPageController = class {
+	constructor() {
+		document.querySelector("#rosefireButton").onclick = (event) => {
+			rhit.AuthManager.signIn();
+		};
+	}
+}
 
-  function elementClickDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    deltaX = prevX - e.clientX;
-    deltaY = prevY - e.clientY;
-    prevX = e.clientX;
-    prevY = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - deltaY) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - deltaX) + "px";
-  }
+rhit.AuthManager = class {
+	constructor() {
+		this._user = null;
+		this._name = "";
+	}
 
-  function elementTouchDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-	touch = e.touches[0];
-    // calculate the new cursor position:
-    deltaX = prevX - touch.clientX;
-    deltaY = prevY - touch.clientY;
-    prevX = touch.clientX;
-    prevY = touch.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - deltaY) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - deltaX) + "px";
-  }
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
 
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-	document.ontouchend = null;
-    document.onmousemove = null;
-	document.ontouchmove = null;
-  }
+	signIn() {
+		console.log("Sign in using Rosefire");
+		Rosefire.signIn("bd7cf5b7-2d50-48c3-8584-9c98090b828d", (err, rfUser) => {
+			if (err) {
+				console.log("Rosefire error!", err);
+				return;
+			}
+			console.log("Rosefire success!", rfUser);
+			this._name = rfUser.name;
+			firebase.auth().signInWithCustomToken(rfUser.token).catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				if (errorCode === 'auth/invalid-custom-token') {
+					alert('The token you provided is not valid.');
+				} else {
+					console.error("Custom auth error", errorCode, errorMessage);
+				}
+			});
+		});
+
+	}
+
+	signOut() {
+		firebase.auth().signOut().catch((error) => {
+			console.log("Sign out error");
+		});
+	}
+
+	get isSignedIn() {
+		return !!this._user;
+	}
+
+	get uid() {
+		return this._user.uid;
+	}
+
+	get name() {
+		return this._name || this._user.displayName;
+	}
+}
+
+rhit.UserManager = class {
+	constructor() {
+		this._collectoinRef = firebase.firestore().collection(rhit.FB_COLLECTION_USERS);
+		this._document = null;
+		this._unsubscribe = null;
+	}
+	addNewUserMaybe(uid, name) {
+		const userRef = this._collectoinRef.doc(uid);
+		return userRef.get().then((doc) => {
+			if (doc.exists) {
+				console.log("User already exists:", doc.data());
+				return false;
+			} else {
+				console.log("Creating this user!");
+				return userRef.set({
+						[rhit.FB_KEY_NAME]: name,
+					})
+					.then(function () {
+						console.log("Document successfully written!");
+						return true;
+					})
+					.catch(function (error) {
+						console.error("Error writing document: ", error);
+					});
+			}
+		}).catch(function (error) {
+			console.log("Error getting document:", error);
+		});
+	}
+	beginListening(uid, changeListener) {
+		const userRef = this._collectoinRef.doc(uid);
+		this._unsubscribe = userRef.onSnapshot((doc) => {
+			if (doc.exists) {
+				console.log("Document data:", doc.data());
+				this._document = doc;
+				changeListener();
+			} else {
+				console.log("No User!  That's bad!");
+			}
+		});
+
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+
+	get isListening() {
+		return !!this._unsubscribe;
+	}
+
+	get name() {
+		return this._document.get(rhit.FB_KEY_NAME);
+	}
+}
+
+rhit.checkForRedirects = function () {
+	if (document.querySelector("#loginPage") && rhit.AuthManager.isSignedIn) {
+		window.location.href = "/index.html";
+	}
+	if (!document.querySelector("#loginPage") && !rhit.AuthManager.isSignedIn) {
+		window.location.href = "/login.html";
+	}
+};
+
+rhit.initializePage = function () {
+	new rhit.SideNavController();
+	if (document.querySelector(".easyzoom")) {
+		rhit.mapPageController = new rhit.MapPageController();
+	}
+	if (document.querySelector("#loginPage")) {
+		new rhit.LoginPageController();
+	}
+};
+
+rhit.createUserObjectIfNeeded = function () {
+	return new Promise((resolve, reject) => {
+		if (!rhit.AuthManager.isSignedIn) {
+			console.log("No user.  So no User check needed");
+			resolve(false);
+			return;
+		}
+		if (!document.querySelector("#loginPage")) {
+			console.log("Not on login page.  So no User check needed");
+			resolve(false);
+			return;
+		}
+		console.log("Checking user");
+		rhit.UserManager.addNewUserMaybe(
+			rhit.AuthManager.uid,
+			rhit.AuthManager.name,
+		).then((isUserNew) => {
+			resolve(isUserNew);
+		});
+	});
 }
 
 function htmlToElement(html){
@@ -192,17 +349,27 @@ rhit.Node = class {
 }
 
 rhit.Annotation = class {
-	constructor(id, room, desc){
-		this.id = id;
-		this.room = room;
+	constructor(author, title, building, desc, position){
+		this.author = author;
+		this.title = title;
+		this.building = building;
 		this.desc = desc;
+		this.position = position;
 	}
 }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
-	rhit.mapPageController = new rhit.MapPageController();
+  rhit.AuthManager = new rhit.AuthManager();
+  rhit.UserManager = new rhit.UserManager();
+  rhit.AuthManager.beginListening(() => {
+		console.log("isSignedIn = ", rhit.AuthManager.isSignedIn);
+		rhit.createUserObjectIfNeeded().then(() => {
+			rhit.checkForRedirects();
+			rhit.initializePage();
+		});
+	});
 };
 
 rhit.main();
